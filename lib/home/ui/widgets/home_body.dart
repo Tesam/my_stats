89cdd90caf39246b09922c1faa81109171f02afd
Tesam <mtsamudiog@gmail.com>
2021-01-07 14:57:41 -0300

    feat(home_body): dynamically add home content

--- lib/home/ui/widgets/home_body.dart
+++ lib/home/ui/widgets/home_body.dart
@@ -1,95 +1,105 @@
 import 'package:flutter/material.dart';
 
 class HomeBody extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return SliverList(
-      delegate: SliverChildListDelegate([
-        SizedBox(
-          height: 16.0,
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Sunday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Monday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Sunday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Monday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Sunday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Monday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Sunday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Monday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Sunday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Monday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Sunday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Monday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Sunday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Monday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Sunday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        ListTile(
-          leading: Icon(Icons.wb_sunny),
-          title: Text('Monday'),
-          subtitle: Text('sunny, h: 80, l: 65'),
-        ),
-        // ListTiles++
-      ]),
+      delegate: SliverChildBuilderDelegate((BuildContext context, int index){
+        return ListTile(
+          leading: Icon(subjects[index].subjectIcon),
+          title: Text(subjects[index].subjectTitle),
+          subtitle: Text(subjects[index].lessonQuantity),
+        );
+      },
+      childCount: subjects.length,
+      ),
     );
   }
 }
+
+class Subject {
+  final String subjectTitle;
+  final String lessonQuantity;
+  final IconData subjectIcon;
+
+  const Subject({
+    this.subjectTitle,
+    this.lessonQuantity,
+    this.subjectIcon
+  });
+
+}
+
+final subjects = [
+  new Subject(
+    subjectTitle: 'Sistemas Operativos II',
+    lessonQuantity: '12 lecciones',
+    subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Álgebra Lineal',
+      lessonQuantity: '10 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+];
