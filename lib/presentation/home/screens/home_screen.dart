9038f35ea94dd82364dfd5da094b0a9bc17cd242
Tesam <mtsamudiog@gmail.com>
2021-06-12 22:50:31 -0400

    feat(home_screen.dart): expose the home bloc to its descendants

--- lib/presentation/home/screens/home_screen.dart
+++ lib/presentation/home/screens/home_screen.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:my_stats/domain/usecases/home_use_case.dart';
+import 'package:my_stats/presentation/home/home_bloc.dart';
 import 'package:my_stats/presentation/home/widgets/home_appbar.dart';
 import 'package:my_stats/presentation/home/widgets/home_body.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
+
+  HomeScreen._();
+
+  static Widget init(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) =>
+          HomeBloc(
+              homeUseCase: context.read<HomeUseCase>()
+          ),
+      builder: (_, __) => HomeScreen._(),
+    );
+  }
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
