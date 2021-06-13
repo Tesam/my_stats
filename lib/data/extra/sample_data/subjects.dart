59883e4cc6261ede4bfadee757952b6504609db2
Tesam <mtsamudiog@gmail.com>
2021-06-12 22:41:43 -0400

    test(subjects.dart): add a subject list object sample data

---
+++ lib/data/extra/sample_data/subjects.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:my_stats/domain/entities/subject.dart';
+
+final List<Subject> subjects = [
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Álgebra Lineal',
+      lessonQuantity: '10 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Bases de Datos',
+      lessonQuantity: '15 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Matemática II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Programación I',
+      lessonQuantity: '20 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Misterio Cristiano',
+      lessonQuantity: '5 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Inglés I',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+  new Subject(
+      subjectTitle: 'Sistemas Operativos II',
+      lessonQuantity: '12 lecciones',
+      subjectIcon: Icons.access_alarm
+  ),
+];
