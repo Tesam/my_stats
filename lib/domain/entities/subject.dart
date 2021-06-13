022463bf5276e866c876d8429fe881aa20126880
Tesam <mtsamudiog@gmail.com>
2021-06-12 22:39:27 -0400

    feat(subject.dart): add the subject entity

---
+++ lib/domain/entities/subject.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Subject {
+  final String subjectTitle;
+  final String lessonQuantity;
+  final IconData subjectIcon;
+
+  const Subject({
+    this.subjectTitle,
+    this.lessonQuantity,
+    this.subjectIcon
+  });
+
+}
