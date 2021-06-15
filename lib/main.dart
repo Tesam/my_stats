c5a762d3997533840d6b14389e476c475e58660d
Tesam <mtsamudiog@gmail.com>
2021-06-15 18:51:44 -0400

    refactor(main.dart): use the dependencies file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_stats/data/repositories/home_api_impl.dart';
-import 'package:my_stats/domain/repositories/home_api_repository.dart';
-import 'package:my_stats/domain/usecases/home_use_case.dart';
+import 'package:my_stats/dependencies.dart';
 import 'package:my_stats/presentation/home/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -14,16 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        Provider<HomeApiRepository>(
-          create: (_) => HomeApiImpl(),
-        ),
-        Provider<HomeUseCase>(
-          create: (context) =>
-              HomeUseCase(context.read<HomeApiRepository>(),
-          ),
-        ),
-      ],
+      providers: dependencies,
       child: Builder(builder: (context) {
         return MaterialApp(
           title: 'Flutter Demo',
