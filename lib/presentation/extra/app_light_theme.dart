be88a572fa1ad26bbdf7edd7b9563c5fbc7ee935
Tesam <mtsamudiog@gmail.com>
2021-07-05 14:44:26 -0400

    feat(app_light_theme.dart): add the ElevatedButtonTheme

--- lib/presentation/extra/app_light_theme.dart
+++ lib/presentation/extra/app_light_theme.dart
@@ -8,7 +8,23 @@ ThemeData appLightTheme(){
         letterSpacing: 2.0,
         color: Colors.white,
         fontWeight: FontWeight.w300
+    ),
+    subtitle1: TextStyle(
+      fontFamily: 'Open Sans',
+      fontSize: 16.0,
+      letterSpacing: 1.0,
+      fontWeight: FontWeight.w400
     )
+  );
+
+  ElevatedButtonThemeData _elevatedButtonTheme = ElevatedButtonThemeData(
+    style: ButtonStyle(
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40.0),
+          )
+      ),
+    ),
   );
 
   return ThemeData(
@@ -16,5 +32,6 @@ ThemeData appLightTheme(){
     colorScheme: ColorScheme.light(),
     primaryColor: Color.fromRGBO(255, 128, 101, 1),
     primaryColorLight: Color.fromRGBO(255, 144, 89, 0.2),
+    elevatedButtonTheme: _elevatedButtonTheme
   );
 }
